8e4e291f59ff3bdf11ca3a32e562f9ca470a0032
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-04-07 00:43:11 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
 import React from "react";
 import './../styles/App.css';
+import Tooltip from "./ToolTip";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <Tooltip text='This is tooltip'>
+        <button >Hover over me</button>
+        </Tooltip>
+        <Tooltip text='This is another tooltip'>
+          <button>Another Tooltip</button>
+          </Tooltip>
     </div>
   )
 }
