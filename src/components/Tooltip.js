4527d515490ffbffd60366eee48aeb2363883f9d
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-04-07 00:43:43 +0530

    Update Tooltip.js

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,1 +1,18 @@
 
+import React, { useState } from 'react'
+
+const Tooltip = ({text,children}) => {
+
+    const [show,setShow]=useState(false);
+    
+  return (
+   <>
+   <div className='tooltip'>
+   <div className='tooltip-text' style={{display:show?'block':'none'}}>{text}</div>
+   <div className='child'  onMouseEnter={()=>setShow(true)} onMouseLeave={()=>setShow(false)}>{children}</div>
+   </div>
+   </>
+  )
+}
+
+export default Tooltip
