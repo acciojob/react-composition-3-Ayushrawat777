5ba11f9498c2804e3ad422fbf40e87ea86dedb56
AYUSH RAWAT <106034287+Ayushrawat777@users.noreply.github.com>
2025-02-20 20:36:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,20 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
 import Tooltip from "./Tooltip";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div >
-        <h2 className="tooltip">
-          <Tooltip text="This is a tooltip">Hover over me</Tooltip>
-        </h2>
-
-        <p className="tooltip">
-          <Tooltip text="This is another tooltip">
-            Hover over me to see another tooltip
-          </Tooltip>
-        </p>
+    <div className="app-container">
+    
+      <Tooltip text="This is a tooltip">
+        <p className="tooltip-p">Hover over me</p>
+      </Tooltip>
+  
+      <Tooltip text="This is another Tooltip">
+        <p className="tooltip-text">Hover over me to see another tooltip</p>
+      </Tooltip>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
